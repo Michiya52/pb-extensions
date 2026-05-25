768c6c8c72fe088d59d8340502cf5e79ac603d66
Ivan <zhengyao.chan@gmail.com>
2026-05-25 15:47:48 +0800

    a

--- 0.8/ComixTo/source.js
+++ 0.8/ComixTo/source.js
@@ -1018,10 +1018,11 @@ var _Sources = (() => {
       return finalChapters;
     }
     parseChapterDetails(data, mangaId, chapterId) {
-      const baseUrl = data.pages.baseUrl ?? "";
-      const pages = data.pages.items.map(
-        (p) => /^https?:\/\//.test(p.url) ? p.url : `${baseUrl}${p.url}`
-      );
+      const baseUrl = (data.pages.baseUrl ?? "").replace(/\/sii?\//, "/i/");
+      const pages = data.pages.items.map((p) => {
+        const url = /^https?:\/\//.test(p.url) ? p.url : `${baseUrl}${p.url}`;
+        return url.replace(/\/sii?\//, "/i/");
+      });
       return App.createChapterDetails({
         id: chapterId,
         mangaId,
