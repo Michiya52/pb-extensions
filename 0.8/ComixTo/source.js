082241c9213d6123c0d4273241f1fafe8738814f
Ivan <zhengyao.chan@gmail.com>
2026-05-26 23:13:16 +0800

    asdad

--- 0.8/ComixTo/source.js
+++ 0.8/ComixTo/source.js
@@ -1730,7 +1730,7 @@ var _Sources = (() => {
 
   // src/ComixTo/ComixHash.ts
   function generateHash(rawPath) {
-    return fastGenerateHash(rawPath);
+    return signPath(rawPath);
   }
   async function decryptComixPayload(rawPath, payload, headers = {}) {
     return fastDecryptComixPayload(rawPath, payload, headers);
