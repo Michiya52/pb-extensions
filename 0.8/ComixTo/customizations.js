c1e1aaed25f701dd8dc93030a7d4bc74128852de
Ivan <zhengyao.chan@gmail.com>
2026-05-31 16:37:52 +0800

    Pull latest upstream update from acepilot147 and bump version to 1.8.2

--- 0.8/ComixTo/customizations.js
+++ 0.8/ComixTo/customizations.js
@@ -960,7 +960,7 @@ const OriginalComixToInfo = _Sources.ComixToInfo;
 
 const NewComixToInfo = {
   ...OriginalComixToInfo,
-  version: "1.8.1",
+  version: "1.8.2",
   author: "Michiya52",
   authorWebsite: "https://github.com/Michiya52",
   description: "Read manga from ComixTo with advanced filters"
